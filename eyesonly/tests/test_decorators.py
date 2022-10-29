b0d6877f6de55b30b2fa27d3e181e4292a3b5e8a
Diego J <diegojromerolopez@gmail.com>
2022-10-29 20:12:38 +0200

    Test decorators

--- eyesonly/tests/test_decorators.py
+++ eyesonly/tests/test_decorators.py
@@ -1,4 +1,5 @@
 import os
+import time
 import unittest
 
 from eyesonly.decorators import eyesonly
@@ -11,7 +12,7 @@ class TestDecorators(unittest.TestCase):
         self.root_path = os.path.dirname(os.path.abspath(__file__))
         Secret.clear_allowed_uses()
         Secret.load_allowed_uses(
-            {os.path.join(self.root_path, 'test_decorators.py'): {'test_secret_type_ok'}}
+            {os.path.join(self.root_path, 'test_decorators.py'): {'test_secret_type_ok', 'run_without_decorator'}}
         )
 
     @eyesonly
@@ -29,3 +30,27 @@ class TestDecorators(unittest.TestCase):
             str(secret)
 
         self.assertEqual('Secret api_key is not allowed to be seen here', str(exc_context.exception))
+
+    def test_performance(self):
+        @eyesonly
+        def run_with_decorator():
+            secret1 = Secret(name='api_key', value='SECRET_API_KEY')
+
+            str(secret1)
+
+        def run_without_decorator():
+            secret2 = Secret(name='api_key', value='SECRET_API_KEY')
+
+            str(secret2)
+
+        run_without_decorator_start_time = time.time()
+        run_without_decorator()
+        run_without_decorator_duration = time.time() - run_without_decorator_start_time
+
+
+        run_with_decorator_start_time = time.time()
+        run_with_decorator()
+        run_with_decorator_duration = time.time() - run_with_decorator_start_time
+
+
+        self.assertLess(run_with_decorator_duration, run_without_decorator_duration)
