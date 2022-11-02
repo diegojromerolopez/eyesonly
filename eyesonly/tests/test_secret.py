378318eb1cb3c099ed72fc5a8bed0ac5b568ec93
Diego J <diegojromerolopez@gmail.com>
2022-11-02 01:02:08 +0100

    Remove performance tests

--- eyesonly/tests/test_secret.py
+++ eyesonly/tests/test_secret.py
@@ -73,26 +73,6 @@ class TestSecret(unittest.TestCase):
 
         self.assertEqual('SECRET_API_KEY', inner_function_in_test())
 
-    def test_secret_performance(self):
-        secret = Secret(name='secret_allowed', acl=self.json_acl, value='SECRET_API_KEY')
-
-        dict_access_start_time = time.time()
-        _ = os.environ['HOME']
-        dict_access_time = time.time() - dict_access_start_time
-
-        str_method_start_time = time.time()
-        value2 = secret.str(__file__, 'test_secret_performance')
-        str_method_time = time.time() - str_method_start_time
-
-        str_cast_start_time = time.time()
-        value3 = str(secret)
-        str_cast_time = time.time() - str_cast_start_time
-
-        self.assertEqual('SECRET_API_KEY', value2)
-        self.assertEqual('SECRET_API_KEY', value3)
-        self.assertAlmostEqual(dict_access_time, str_method_time, delta=10e-5)
-        self.assertAlmostEqual(str_cast_time, str_method_time, delta=10e-2)
-
     def test_toml_config_secret_not_allowed(self):
         secret = Secret(name='secret_not_allowed_anywhere', value='SECRET_API_KEY', acl=self.toml_acl)
         with self.assertRaises(EyesOnlyException) as exc_context:
